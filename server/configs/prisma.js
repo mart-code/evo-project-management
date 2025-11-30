fc679adc718d312c462f3bd06f540809823de9fa
mart-code <aamadewoyin@gmail.com>
2025-11-30 21:56:17 +0100

    node env fix

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -12,6 +12,6 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({connectionString});
 const prisma = global.prisma || new PrismaClient({adapter});
 
-if(process.env.NODE_ENV === 'developmment') global.prisma = prisma;
+if(process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
